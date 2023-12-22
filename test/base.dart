c261a7732c255cc95b72e84c453c7e26cef917d1
Franklin <franklin.carrero@conekta.com>
2023-12-22 16:27:02 -0500

    set meta version

--- test/base.dart
+++ test/base.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 class Base {
-  static final String basePath = Platform.environment['BASE_PATH'] != null
-      ? Platform.environment['BASE_PATH']
-      : 'http://localhost:3000';
+  static final String basePath =
+      Platform.environment['BASE_PATH'] ?? 'http://localhost:3000';
 }
