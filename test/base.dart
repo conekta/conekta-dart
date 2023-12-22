ba15804f2f4e0b44f211b4aa1e3587f1e3b13897
Franklin <franklin.carrero@conekta.com>
2023-12-22 12:25:49 -0500

    new entities

--- test/base.dart
+++ test/base.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 class Base {
-  static final String basePath =
-      Platform.environment['BASE_PATH'] ?? 'http://localhost:3000';
+  static final String basePath = Platform.environment['BASE_PATH'] != null
+      ? Platform.environment['BASE_PATH']
+      : 'http://localhost:3000';
 }
--- test/tokens_api_test.dart
+++ test/tokens_api_test.dart
@@ -1,20 +1,34 @@
 import 'package:test/test.dart';
 import 'package:conekta/conekta.dart';
-
+import 'base.dart';
 
 /// tests for TokensApi
 void main() {
-  final instance = Conekta().getTokensApi();
+  var instance = Conekta(basePathOverride: Base.basePath);
+  instance.setBearerAuth("bearerAuth", "key_xxxx");
+  final api = instance.getTokensApi();
 
   group(TokensApi, () {
     // Create Token
     //
-    // Generate a payment token, to associate it with a card 
+    // Generate a payment token, to associate it with a card
     //
     //Future<TokenResponse> createToken(Token token, { String acceptLanguage }) async
     test('test createToken', () async {
-      // TODO
-    });
+      var tokenCard = TokenCard((b) => b
+        ..cvc = '123'
+        ..deviceFingerprint = 'device_fingerprint_value'
+        ..expMonth = '12'
+        ..expYear = '2023'
+        ..name = 'Nombre del Titular'
+        ..number = '5475040095304607');
 
+      var token = Token((b) => b..card.replace(tokenCard));
+
+      final response = await api.createToken(token: token);
+
+      expect(response, isNotNull);
+      expect("tok_2toPJUcZ27AH5LsZk", response.data?.id);
+    });
   });
 }
