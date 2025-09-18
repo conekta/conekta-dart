d3f36c6e1b60c94b731586103abff6b25aea5464
Martín Agüera <maguera@conekta.com>
2025-09-18 15:50:35 -0300

    chore: [AI-453] - changed version of 7.0.2 to 7.0.5

--- lib/src/api/companies_api.dart
+++ lib/src/api/companies_api.dart
@@ -67,7 +67,7 @@ class CompaniesApi {
     final _options = Options(
       method: r'GET',
       headers: <String, dynamic>{
-        r'User-Agent': r'Conekta/v2 DartBindings/7.0.2',
+        r'User-Agent': r'Conekta/v2 DartBindings/7.0.5',
         if (acceptLanguage != null) r'Accept-Language': acceptLanguage,
         if (localVarAccept != null) r'Accept': localVarAccept,
         if (localVarContentType != null) r'Content-Type': localVarContentType,
@@ -173,7 +173,7 @@ class CompaniesApi {
     final _options = Options(
       method: r'GET',
       headers: <String, dynamic>{
-        r'User-Agent': r'Conekta/v2 DartBindings/7.0.2',
+        r'User-Agent': r'Conekta/v2 DartBindings/7.0.5',
         if (acceptLanguage != null) r'Accept-Language': acceptLanguage,
         if (localVarAccept != null) r'Accept': localVarAccept,
         if (localVarContentType != null) r'Content-Type': localVarContentType,
--- lib/src/api/tokens_api.dart
+++ lib/src/api/tokens_api.dart
@@ -61,7 +61,7 @@ class TokensApi {
     final _options = Options(
       method: r'POST',
       headers: <String, dynamic>{
-        r'User-Agent': r'Conekta/v2 DartBindings/7.0.2',
+        r'User-Agent': r'Conekta/v2 DartBindings/7.0.5',
         if (acceptLanguage != null) r'Accept-Language': acceptLanguage,
         if (localVarAccept != null) r'Accept': localVarAccept,
         if (localVarContentType != null) r'Content-Type': localVarContentType,
